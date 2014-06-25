f4bfd8e0caf02ae357d090b8dfa2196bd5b8e69a
David Couturier <david.couturier@polymtl.ca>
2014-06-24 20:38:57 -0700

    WIP Gets Seg. Fault on clGetPlatformInfo...

--- clust.c
+++ clust.c
@@ -11,14 +11,39 @@
 #include <GL/gl.h>
 
 #include "clust.h"
-int var = 0;
+
+cl_api_call_clGetPlatformIDs libCL_clGetPlatformIDs;
+cl_api_call_clGetPlatformInfo libCL_clGetPlatformInfo;
+cl_api_call_clGetDeviceIDs libCL_clGetDeviceIDs;
+cl_api_call_clGetDeviceInfo libCL_clGetDeviceInfo;
+cl_api_call_clCreateContext libCL_clCreateContext;
+cl_api_call_clCreateContextFromType libCL_clCreateContextFromType;
+cl_api_call_clRetainContext libCL_clRetainContext;
+cl_api_call_clReleaseContext libCL_clReleaseContext;
+cl_api_call_clGetContextInfo libCL_clGetContextInfo;
+cl_api_call_clCreateCommandQueue libCL_clCreateCommandQueue;
+cl_api_call_clRetainCommandQueue libCL_clRetainCommandQueue;
+cl_api_call_clReleaseCommandQueue libCL_clReleaseCommandQueue;
+cl_api_call_clGetCommandQueueInfo libCL_clGetCommandQueueInfo;
 
 __attribute__((constructor)) void libCLUST() {
-	printf("hello lib!\n");
+#ifdef DEBUG
+	fprintf(stdout, "%s: OpenCL Symbols being replaced...\n", LIB_NAME);
+#endif
 	init_libcl_symbols();
 }
 
-static void init_libcl_symbols() {
+void* dlSymFunction(void* libPtr, const char* functionName) {
+	void* ptr;
+	*(void**)(&ptr) = dlsym(libPtr, LIBCL_GET_PLATFORM_IDS_STR);
+	if(!ptr) {
+		fprintf(stderr, "%s: Unable to load %s", LIB_NAME, functionName);
+		exit(EXIT_FAILURE);
+	}
+	return ptr;
+}
+
+void init_libcl_symbols() {
 	void* libcl_ptr;
 
 	dlerror();
@@ -30,19 +55,118 @@ static void init_libcl_symbols() {
 	}
 
 	dlerror();
-	*(void**)(&libCL_clGetDeviceIDs) = dlsym(libcl_ptr, LIBCL_GET_DEVICE_IDS_STR);
-	if(!libCL_clGetDeviceIDs) {
-		fprintf(stderr, "%s: Unable to load %s", LIB_NAME, LIBCL_GET_DEVICE_IDS_STR);
-		exit(EXIT_FAILURE);
-	}
+	libCL_clGetPlatformIDs = (cl_api_call_clGetPlatformIDs) dlSymFunction(libcl_ptr, LIBCL_GET_PLATFORM_IDS_STR);
+	libCL_clGetPlatformInfo = (cl_api_call_clGetPlatformInfo) dlSymFunction(libcl_ptr, LIBCL_GET_PLATFORM_INFO_STR);
+	libCL_clGetDeviceIDs = (cl_api_call_clGetDeviceIDs) dlSymFunction(libcl_ptr, LIBCL_GET_DEVICE_IDS_STR);
+	libCL_clGetDeviceInfo = (cl_api_call_clGetDeviceInfo) dlSymFunction(libcl_ptr, LIBCL_GET_DEVICE_INFO_STR);
+	libCL_clCreateContext = (cl_api_call_clCreateContext) dlSymFunction(libcl_ptr, LIBCL_CREATE_CONTEXT_STR);
+	libCL_clCreateContextFromType = (cl_api_call_clCreateContextFromType) dlSymFunction(libcl_ptr, LIBCL_CREATE_CONTEXT_FROM_TYPE_STR);
+	libCL_clRetainContext = (cl_api_call_clRetainContext) dlSymFunction(libcl_ptr, LIBCL_RETAIN_CONTEXT_STR);
+	libCL_clReleaseContext = (cl_api_call_clReleaseContext) dlSymFunction(libcl_ptr, LIBCL_RELEASE_CONTEXT_STR);
+	libCL_clGetContextInfo = (cl_api_call_clGetContextInfo) dlSymFunction(libcl_ptr, LIBCL_GET_CONTEXT_INFO_STR);
+	libCL_clCreateCommandQueue = (cl_api_call_clCreateCommandQueue) dlSymFunction(libcl_ptr, LIBCL_CREATE_COMMAND_QUEUE_STR);
+	libCL_clRetainCommandQueue = (cl_api_call_clRetainCommandQueue) dlSymFunction(libcl_ptr, LIBCL_RETAIN_COMMAND_QUEUE_STR);
+	libCL_clReleaseCommandQueue = (cl_api_call_clReleaseCommandQueue) dlSymFunction(libcl_ptr, LIBCL_RELEASE_COMMAND_QUEUE_STR);
+	libCL_clGetCommandQueueInfo = (cl_api_call_clGetCommandQueueInfo) dlSymFunction(libcl_ptr, LIBCL_GET_COMMAND_QUEUE_STR);
+//	libCL_cl = (cl_api_call_cl) dlsymFunction(libcl_ptr, LIBCL_);
 
-	//	arbitrary libCL_clGetDeviceIDs;
-	//	*(void**)(&libCL_clGetDeviceIDs) = dlsym(libcl_ptr, LIBCL_GET_DEVICE_IDS_STR);
-	//	libCL_clGetDeviceIDs();
 #ifdef DEBUG
-	fprintf(stdout, "symbols initialized!\n");
+	fprintf(stdout, "%s: Symbols initialized!\n", LIB_NAME);
 #endif
 }
+
+CL_API_ENTRY cl_int CL_API_CALL clGetPlatformIDs(cl_uint num_entries, cl_platform_id * platforms, cl_uint * num_platforms) CL_API_SUFFIX__VERSION_1_0 {
+#ifdef DEBUG
+	fprintf(stdout, "%s: %s\n", LIB_NAME, LIBCL_GET_PLATFORM_IDS_STR);
+	fflush(stdout);
+#endif
+	return libCL_clGetPlatformIDs(num_entries,platforms,num_platforms);
+}
+CL_API_ENTRY cl_int CL_API_CALL clGetPlatformInfo(cl_platform_id platform, cl_platform_info param_name, size_t param_value_size, void * param_value, size_t * param_value_size_ret) CL_API_SUFFIX__VERSION_1_0 {
+#ifdef DEBUG
+	fprintf(stdout, "%s: %s(%d, %s, %d, %d, %d)\n", LIB_NAME, LIBCL_GET_PLATFORM_INFO_STR, platform, param_name, param_value_size, param_value, *param_value_size_ret);
+	fflush(stdout);
+#endif
+	return libCL_clGetPlatformInfo(platform, param_name, param_value_size, &param_value, param_value_size_ret);
+}
+CL_API_ENTRY cl_int CL_API_CALL clGetDeviceIDs(cl_platform_id platform, cl_device_type device_type, cl_uint num_entries, cl_device_id * devices, cl_uint * num_devices) CL_API_SUFFIX__VERSION_1_0 {
+#ifdef DEBUG
+	fprintf(stdout, "%s: %s\n", LIB_NAME, LIBCL_GET_DEVICE_IDS_STR);
+	fflush(stdout);
+#endif
+	return libCL_clGetDeviceIDs(platform, device_type, num_entries, devices, num_devices);
+}
+CL_API_ENTRY cl_int CL_API_CALL clGetDeviceInfo(cl_device_id device, cl_device_info param_name, size_t param_value_size, void * param_value, size_t * param_value_size_ret ) CL_API_SUFFIX__VERSION_1_0 {
+#ifdef DEBUG
+	fprintf(stdout, "%s: %s\n", LIB_NAME, LIBCL_GET_DEVICE_INFO_STR);
+	fflush(stdout);
+#endif
+	return libCL_clGetDeviceInfo(device, param_name, param_value_size, param_value, param_value_size_ret);
+}
+CL_API_ENTRY cl_context CL_API_CALL clCreateContext(const cl_context_properties * properties, cl_uint num_devices, const cl_device_id * devices, void (CL_CALLBACK * pfn_notify )(const char *, const void *, size_t, void *), void * user_data, cl_int * errcode_ret ) CL_API_SUFFIX__VERSION_1_0 {
+#ifdef DEBUG
+	fprintf(stdout, "%s: %s\n", LIB_NAME, LIBCL_CREATE_CONTEXT_STR);
+	fflush(stdout);
+#endif
+	return libCL_clCreateContext(properties, num_devices, devices, pfn_notify, user_data, errcode_ret);
+}
+CL_API_ENTRY cl_context CL_API_CALL clCreateContextFromType(const cl_context_properties * properties, cl_device_type device_type, void (CL_CALLBACK * pfn_notify )(const char *, const void *, size_t, void *), void * user_data, cl_int * errcode_ret ) CL_API_SUFFIX__VERSION_1_0 {
+#ifdef DEBUG
+	fprintf(stdout, "%s: %s\n", LIB_NAME, LIBCL_CREATE_CONTEXT_FROM_TYPE_STR);
+	fflush(stdout);
+#endif
+	return libCL_clCreateContextFromType(properties, device_type, pfn_notify, user_data, errcode_ret);
+}
+CL_API_ENTRY cl_int CL_API_CALL clRetainContext(cl_context context ) CL_API_SUFFIX__VERSION_1_0 {
+#ifdef DEBUG
+	fprintf(stdout, "%s: %s\n", LIB_NAME, LIBCL_RETAIN_CONTEXT_STR);
+	fflush(stdout);
+#endif
+	return libCL_clRetainContext(context);
+}
+CL_API_ENTRY cl_int CL_API_CALL clReleaseContext(cl_context context ) CL_API_SUFFIX__VERSION_1_0 {
+#ifdef DEBUG
+	fprintf(stdout, "%s: %s\n", LIB_NAME, LIBCL_RELEASE_CONTEXT_STR);
+	fflush(stdout);
+#endif
+	return libCL_clReleaseContext(context);
+}
+CL_API_ENTRY cl_int CL_API_CALL clGetContextInfo(cl_context context, cl_context_info param_name, size_t param_value_size, void * param_value, size_t * param_value_size_ret ) CL_API_SUFFIX__VERSION_1_0 {
+#ifdef DEBUG
+	fprintf(stdout, "%s: %s\n", LIB_NAME, LIBCL_GET_CONTEXT_INFO_STR);
+	fflush(stdout);
+#endif
+	return libCL_clGetContextInfo(context, param_name, param_value_size, param_value, param_value_size_ret);
+}
+CL_API_ENTRY cl_command_queue CL_API_CALL clCreateCommandQueue(cl_context context, cl_device_id device, cl_command_queue_properties properties, cl_int * errcode_ret ) CL_API_SUFFIX__VERSION_1_0 {
+#ifdef DEBUG
+	fprintf(stdout, "%s: %s\n", LIB_NAME, LIBCL_CREATE_COMMAND_QUEUE_STR);
+	fflush(stdout);
+#endif
+	return libCL_clCreateCommandQueue(context, device, properties, errcode_ret);
+}
+CL_API_ENTRY cl_int CL_API_CALL clRetainCommandQueue(cl_command_queue command_queue ) CL_API_SUFFIX__VERSION_1_0 {
+#ifdef DEBUG
+	fprintf(stdout, "%s: %s\n", LIB_NAME, LIBCL_RETAIN_COMMAND_QUEUE_STR);
+	fflush(stdout);
+#endif
+	return libCL_clRetainCommandQueue(command_queue);
+}
+CL_API_ENTRY cl_int CL_API_CALL clReleaseCommandQueue(cl_command_queue command_queue ) CL_API_SUFFIX__VERSION_1_0 {
+#ifdef DEBUG
+	fprintf(stdout, "%s: %s\n", LIB_NAME, LIBCL_RELEASE_COMMAND_QUEUE_STR);
+	fflush(stdout);
+#endif
+	return libCL_clReleaseCommandQueue(command_queue);
+}
+CL_API_ENTRY cl_int CL_API_CALL clGetCommandQueueInfo(cl_command_queue command_queue, cl_command_queue_info param_name, size_t param_value_size, void * param_value, size_t * param_value_size_ret ) CL_API_SUFFIX__VERSION_1_0 {
+#ifdef DEBUG
+	fprintf(stdout, "%s: %s\n", LIB_NAME, LIBCL_GET_COMMAND_QUEUE_STR);
+	fflush(stdout);
+#endif
+	return libCL_clGetCommandQueueInfo(command_queue, param_name, param_value_size, param_value, param_value_size_ret);
+}
+
 
 void doStuff() {
 	printf("doing stuff!\n");
--- clust.h
+++ clust.h
@@ -11,34 +11,204 @@
 #include <CL/cl.h>
 
 #define LIB_NAME "libCLUST"
-#define LIBCL_NAME "libGL.so"
+#define LIBCL_NAME "libOpenCL.so"
 
 #define LIBCL_GET_PLATFORM_IDS_STR "clGetPlatformIDs"
+#define LIBCL_GET_PLATFORM_INFO_STR "clGetPlatformInfo"
 #define LIBCL_GET_DEVICE_IDS_STR "clGetDeviceIDs"
+#define LIBCL_GET_DEVICE_INFO_STR "clGetDeviceInfo"
+#define LIBCL_CREATE_CONTEXT_STR "clCreateContext"
+#define LIBCL_CREATE_CONTEXT_FROM_TYPE_STR "clCreateContextFromType"
+#define LIBCL_RETAIN_CONTEXT_STR "clRetainContext"
+#define LIBCL_RELEASE_CONTEXT_STR "clReleaseContext"
+#define LIBCL_GET_CONTEXT_INFO_STR "clGetContextInfo"
+#define LIBCL_CREATE_COMMAND_QUEUE_STR "clCreateCommandQueue"
+#define LIBCL_RETAIN_COMMAND_QUEUE_STR "clRetainCommandQueue"
+#define LIBCL_RELEASE_COMMAND_QUEUE_STR "clReleaseCommandQueue"
+#define LIBCL_GET_COMMAND_QUEUE_STR "clGetCommandQueueInfo"
+//#define LIBCL__STR ""
+//#define LIBCL__STR ""
+//#define LIBCL__STR ""
 
-// OpenCL version 1.2: https://www.khronos.org/registry/cl/api/1.2/cl.h
+// OpenCL version 1.1: https://www.khronos.org/registry/cl/api/1.1/cl.h
+/* Platform API */
+typedef CL_API_ENTRY cl_int CL_API_CALL (*cl_api_call_clGetPlatformIDs) (
+		cl_uint          /* num_entries */,
+		cl_platform_id * /* platforms */,
+		cl_uint *        /* num_platforms */) CL_API_SUFFIX__VERSION_1_0;
 
-typedef void* (*cl_api_call_clGetPlatformIDs)(cl_uint          /* num_entries */,
-        cl_platform_id * /* platforms */,
-        cl_uint *        /* num_platforms */) CL_API_SUFFIX__VERSION_1_0;
+extern cl_api_call_clGetPlatformIDs libCL_clGetPlatformIDs;
+extern CL_API_ENTRY cl_int CL_API_CALL
+clGetPlatformIDs(cl_uint          /* num_entries */,
+		cl_platform_id * /* platforms */,
+		cl_uint *        /* num_platforms */) CL_API_SUFFIX__VERSION_1_0;
 
-typedef void* (*cl_api_call_clGetPlatformInfo)(cl_platform_id   /* platform */,
-        cl_platform_info /* param_name */,
-        size_t           /* param_value_size */,
-        void *           /* param_value */,
-        size_t *         /* param_value_size_ret */) CL_API_SUFFIX__VERSION_1_0;
 
-typedef void* (*cl_api_call_clGetDeviceIDs)(cl_platform_id   /* platform */,
-        cl_device_type   /* device_type */,
-        cl_uint          /* num_entries */,
-        cl_device_id *   /* devices */,
-        cl_uint *        /* num_devices */) CL_API_SUFFIX__VERSION_1_0;
 
-cl_api_call_clGetPlatformIDs libCL_clGetPlatformIDs;
-cl_api_call_clGetPlatformInfo libCL_clGetPlatformInfo;
-cl_api_call_clGetDeviceIDs libCL_clGetDeviceIDs;
+typedef CL_API_ENTRY cl_int CL_API_CALL (*cl_api_call_clGetPlatformInfo) (
+		cl_platform_id   /* platform */,
+		cl_platform_info /* param_name */,
+		size_t           /* param_value_size */,
+		void *           /* param_value */,
+		size_t *         /* param_value_size_ret */) CL_API_SUFFIX__VERSION_1_0;
+extern cl_api_call_clGetPlatformInfo libCL_clGetPlatformInfo;
+extern CL_API_ENTRY cl_int CL_API_CALL
+clGetPlatformInfo(cl_platform_id   /* platform */,
+                  cl_platform_info /* param_name */,
+                  size_t           /* param_value_size */,
+                  void *           /* param_value */,
+                  size_t *         /* param_value_size_ret */) CL_API_SUFFIX__VERSION_1_0;
 
-static void init_libcl_symbols();
-void doStuff();
+
+
+/* Device APIs */
+typedef CL_API_ENTRY cl_int CL_API_CALL (*cl_api_call_clGetDeviceIDs) (
+		cl_platform_id   /* platform */,
+		cl_device_type   /* device_type */,
+		cl_uint          /* num_entries */,
+		cl_device_id *   /* devices */,
+		cl_uint *        /* num_devices */) CL_API_SUFFIX__VERSION_1_0;
+extern cl_api_call_clGetDeviceIDs libCL_clGetDeviceIDs;
+extern CL_API_ENTRY cl_int CL_API_CALL
+clGetDeviceIDs(cl_platform_id   /* platform */,
+               cl_device_type   /* device_type */,
+               cl_uint          /* num_entries */,
+               cl_device_id *   /* devices */,
+               cl_uint *        /* num_devices */) CL_API_SUFFIX__VERSION_1_0;
+
+
+
+typedef CL_API_ENTRY cl_int CL_API_CALL (*cl_api_call_clGetDeviceInfo) (
+		cl_device_id    /* device */,
+		cl_device_info  /* param_name */,
+		size_t          /* param_value_size */,
+		void *          /* param_value */,
+		size_t *        /* param_value_size_ret */) CL_API_SUFFIX__VERSION_1_0;
+extern cl_api_call_clGetDeviceInfo libCL_clGetDeviceInfo;
+extern CL_API_ENTRY cl_int CL_API_CALL
+clGetDeviceInfo(cl_device_id    /* device */,
+                cl_device_info  /* param_name */,
+                size_t          /* param_value_size */,
+                void *          /* param_value */,
+                size_t *        /* param_value_size_ret */) CL_API_SUFFIX__VERSION_1_0;
+
+
+
+
+/* Context APIs  */
+typedef CL_API_ENTRY cl_context CL_API_CALL (*cl_api_call_clCreateContext) (
+		const cl_context_properties * /* properties */,
+		cl_uint                       /* num_devices */,
+		const cl_device_id *          /* devices */,
+		void (CL_CALLBACK * /* pfn_notify */)(const char *, const void *, size_t, void *),
+		void *                        /* user_data */,
+		cl_int *                      /* errcode_ret */) CL_API_SUFFIX__VERSION_1_0;
+extern cl_api_call_clCreateContext libCL_clCreateContext;
+extern CL_API_ENTRY cl_context CL_API_CALL
+clCreateContext(const cl_context_properties * /* properties */,
+                cl_uint                       /* num_devices */,
+                const cl_device_id *          /* devices */,
+                void (CL_CALLBACK * /* pfn_notify */)(const char *, const void *, size_t, void *),
+                void *                        /* user_data */,
+                cl_int *                      /* errcode_ret */) CL_API_SUFFIX__VERSION_1_0;
+
+
+
+typedef CL_API_ENTRY cl_context CL_API_CALL (*cl_api_call_clCreateContextFromType) (
+		const cl_context_properties * /* properties */,
+		cl_device_type                /* device_type */,
+		void (CL_CALLBACK *     /* pfn_notify*/ )(const char *, const void *, size_t, void *),
+		void *                        /* user_data */,
+		cl_int *                      /* errcode_ret */) CL_API_SUFFIX__VERSION_1_0;
+extern cl_api_call_clCreateContextFromType libCL_clCreateContextFromType;
+extern CL_API_ENTRY cl_context CL_API_CALL
+clCreateContextFromType(const cl_context_properties * /* properties */,
+                        cl_device_type                /* device_type */,
+                        void (CL_CALLBACK *     /* pfn_notify*/ )(const char *, const void *, size_t, void *),
+                        void *                        /* user_data */,
+                        cl_int *                      /* errcode_ret */) CL_API_SUFFIX__VERSION_1_0;
+
+
+
+typedef CL_API_ENTRY cl_int CL_API_CALL (*cl_api_call_clRetainContext) (
+		cl_context /* context */) CL_API_SUFFIX__VERSION_1_0;
+extern cl_api_call_clRetainContext libCL_clRetainContext;
+extern CL_API_ENTRY cl_int CL_API_CALL
+clRetainContext(cl_context /* context */) CL_API_SUFFIX__VERSION_1_0;
+
+
+
+typedef CL_API_ENTRY cl_int CL_API_CALL (*cl_api_call_clReleaseContext) (
+		cl_context /* context */) CL_API_SUFFIX__VERSION_1_0;
+extern cl_api_call_clReleaseContext libCL_clReleaseContext;
+extern CL_API_ENTRY cl_int CL_API_CALL
+clReleaseContext(cl_context /* context */) CL_API_SUFFIX__VERSION_1_0;
+
+
+
+typedef CL_API_ENTRY cl_int CL_API_CALL (*cl_api_call_clGetContextInfo) (
+		cl_context         /* context */,
+		cl_context_info    /* param_name */,
+		size_t             /* param_value_size */,
+		void *             /* param_value */,
+		size_t *           /* param_value_size_ret */) CL_API_SUFFIX__VERSION_1_0;
+extern cl_api_call_clGetContextInfo libCL_clGetContextInfo;
+extern CL_API_ENTRY cl_int CL_API_CALL
+clGetContextInfo(cl_context         /* context */,
+                 cl_context_info    /* param_name */,
+                 size_t             /* param_value_size */,
+                 void *             /* param_value */,
+                 size_t *           /* param_value_size_ret */) CL_API_SUFFIX__VERSION_1_0;
+
+
+
+/* Command Queue APIs */
+typedef CL_API_ENTRY cl_command_queue CL_API_CALL (*cl_api_call_clCreateCommandQueue) (
+		cl_context                     /* context */,
+		cl_device_id                   /* device */,
+		cl_command_queue_properties    /* properties */,
+		cl_int *                       /* errcode_ret */) CL_API_SUFFIX__VERSION_1_0;
+extern cl_api_call_clCreateCommandQueue libCL_clCreateCommandQueue;
+extern CL_API_ENTRY cl_command_queue CL_API_CALL
+clCreateCommandQueue(cl_context                     /* context */,
+                     cl_device_id                   /* device */,
+                     cl_command_queue_properties    /* properties */,
+                     cl_int *                       /* errcode_ret */) CL_API_SUFFIX__VERSION_1_0;
+
+
+
+typedef CL_API_ENTRY cl_int CL_API_CALL (*cl_api_call_clRetainCommandQueue) (
+		cl_command_queue /* command_queue */) CL_API_SUFFIX__VERSION_1_0;
+extern cl_api_call_clRetainCommandQueue libCL_clRetainCommandQueue;
+extern CL_API_ENTRY cl_int CL_API_CALL
+clRetainCommandQueue(cl_command_queue /* command_queue */) CL_API_SUFFIX__VERSION_1_0;
+
+
+
+typedef CL_API_ENTRY cl_int CL_API_CALL (*cl_api_call_clReleaseCommandQueue) (
+		cl_command_queue /* command_queue */) CL_API_SUFFIX__VERSION_1_0;
+extern cl_api_call_clReleaseCommandQueue libCL_clReleaseCommandQueue;
+extern CL_API_ENTRY cl_int CL_API_CALL
+clReleaseCommandQueue(cl_command_queue /* command_queue */) CL_API_SUFFIX__VERSION_1_0;
+
+
+
+typedef CL_API_ENTRY cl_int CL_API_CALL (*cl_api_call_clGetCommandQueueInfo) (
+		cl_command_queue      /* command_queue */,
+		cl_command_queue_info /* param_name */,
+		size_t                /* param_value_size */,
+		void *                /* param_value */,
+		size_t *              /* param_value_size_ret */) CL_API_SUFFIX__VERSION_1_0;
+extern cl_api_call_clGetCommandQueueInfo libCL_clGetCommandQueueInfo;
+extern CL_API_ENTRY cl_int CL_API_CALL
+clGetCommandQueueInfo(cl_command_queue      /* command_queue */,
+                      cl_command_queue_info /* param_name */,
+                      size_t                /* param_value_size */,
+                      void *                /* param_value */,
+                      size_t *              /* param_value_size_ret */) CL_API_SUFFIX__VERSION_1_0;
+
+
+
+extern void init_libcl_symbols();
 
 #endif /* CLUST_H_ */
